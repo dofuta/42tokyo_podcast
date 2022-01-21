b96202a030ad53186072433c2fa0c73851b232db
kenta takahashi <k.takahashi@monopo.co.jp>
2022-01-22 01:21:24 +0900

    copy edit

--- components/index/TopArea.vue
+++ components/index/TopArea.vue
@@ -6,6 +6,10 @@
     <p class="toparea__exact">
       <span class='bold'>Hello Wolrd - 42 Tokyo</span> は、<br class='only_sp'/><span class='bold'>プログラミング</span>と<span class='bold'>42 Tokyo</span>の<br class='only_sp'/>そこ知れぬ魅力に迫るべく、<br class='only_sp'/>
       生徒の我々が勝手に始めた<br class='only_sp'/>皆さんと作るラジオ番組です。
+      <br><br>
+
+      <span class='bold'>プログラミング</span>や<span class='bold'>42Tokyo</span>に興味があるみなさん、42現役学生のみなさんに向けて、毎回ゲストをお招きし、<span class='bold'>プログラミングの世界</span>との出会いから、<span class='bold'>これからの世界</span>の話まで、ゆるりと話していきます。
+
     </p>
     <div class="toparea__desc">
       <div class="item" v-for="item in items" :key="item.title">
@@ -25,16 +29,16 @@ export default {
     return {
       items: [
         {
-          title: "THE FIRST TOUCH<br class='only_sp'/> 〜 初めてプログラミングに触れた瞬間",
-          desc: 'ゲストにお呼びしたプログラマー・エンジニアが初めてプログラミングに触れた瞬間を深ぼっていく企画です。'
+          title: "THE FIRST TOUCH<br class='only_sp'/> 〜初めてプログラミングに触れた瞬間〜",
+          desc: 'ゲストにお呼びしたプログラマー・エンジニアが初めてプログラミングに触れた時のエピソードを伺っていきます。どんなエンジニアも初めはエンジニアじゃなかった。'
         },
         {
-          title: '今年のベストバイ・コマンド',
-          desc: '知って良かったコマンドから、便利なツールやライブラリ、アプリ、はたまた日用品まで、ゲストの「ベストバイ」を聞いていく企画です。',
+          title: '今年のベストバイ・コマンド!',
+          desc: '知って良かったコマンドから、便利なツールやライブラリ、アプリ、はたまた日用品まで、ゲストの「ベストバイ」を聞いていく企画です。知らなかった便利tipsが見つかるかも?',
         },
         {
-          title: '42について',
-          desc: '42 Tokyoの学生や、外部ゲストに、42について質問していきます。',
+          title: '42Tokyoのハナシ',
+          desc: '42Tokyoでの学生生活から、入学を決めたきっかけ、今後の活動まで伺っていきます。謎の多いエンジニア養成機関、42 Tokyoのリアルな学生生活に迫ります。',
         },
       ]
     }
